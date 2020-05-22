cde559b59a7f2f2d85d5a7d62fe07d15fabf176c
Heine Olsson Aabø <heine2307@eduroam-193-157-161-138.wlan.uio.no>
2020-05-22 11:01:23 +0200

    Circuit list and pauli string classes

---
+++ quantum_circuit/circuit_list.py
@@ -0,0 +1,185 @@
+import numpy as np
+from .gates import X,Y,Z
+
+class PauliString:
+    def __init__(self,factor,gates,qubits):
+        self.factor = factor
+        self.qubits = qubits
+        self.gates = gates
+        
+
+    def prepare(self,qc,qb):
+        for gate,qubit in zip(self.gates,self.qubits):
+            if gate.char != 'Z':
+                qc = gate.to_qiskit(qc,qubit,transform=True)
+                #qc.append(gate.to_qiskit(transform=True),[qubit],[])
+        return qc
+   
+    def expectation(self,result,shots):
+        """ Interpret result from qiskit measurement. """
+        E = 0
+        for state,num_measure in result.items():
+            state = state[::-1]
+            eigval = 1
+            for gate,qubit in zip(self.gates,self.qubits):
+                if state[qubit] == '1':
+                    eigval *= -1
+            E += eigval*num_measure
+        E /= shots
+        #print(self,E*self.factor,result)
+        return E*self.factor
+
+    def __str__(self):
+        return str([self.factor,[gate.char+str(i) for gate,i in zip(self.gates,self.qubits)]])
+
+    def __eq__(self,other):
+        """ Equality except factor """
+        if not isinstance(other,PauliString):
+            return False
+        if len(self.gates) != len(other.gates):
+            return False
+        if self.gates == other.gates and self.qubits == other.qubits:
+            return True
+        return False
+
+    def __len__(self):
+        return len(self.gates)
+
+class QWCGroup:
+    """
+        Qubit-wise commuting group of Pauli strings.
+    """
+    def __init__(self,paulis=None):
+        self.paulis = paulis
+        if paulis == None:
+            self.paulis = []
+
+    def prepare(self,qc,qb):
+        for pauli_string in self.paulis:
+            qc = pauli_string.prepare(qc,qb)
+        return qc
+
+    def expectation(self,result,shots):
+        E = 0
+        for pauli_string in self.paulis:
+            E += pauli_string.expectation(result,shots)
+        return E
+    
+    def append(self,pauli,check_equal=False):
+        if check_equal:
+            if pauli in self.paulis:
+                i = self.paulis.index(pauli)
+                self.paulis[i].factor += pauli.factor
+            else:
+                self.paulis.append(pauli)
+        else:
+            self.paulis.append(pauli)
+
+    def __str__(self):
+        string = ''
+        for pauli in sorted(self.paulis,key=len):
+            string += str(pauli)+'\n'
+        return string[:-1]
+
+    def __getitem__(self,i):
+        return self.paulis[i]
+
+    def __len__(self):
+        return len(self.paulis)
+
+    def __eq__(self,other):
+        return False
+
+class GCGroup:
+    """
+        General commuting group of Pauli strings.
+    """
+    def __init__(self,paulis):
+        self.paulis = paulis
+
+    def prepare(self,qc,qb):
+        return self.ansatz(qc,qb)
+    
+    def set_ansatz(self,ansatz,outcomes):
+        """
+            Set ansatz for simultaneous eigenbasis of the commuting operators.
+            Outcomes - what eigenstates corresponds to what operators.
+                -> Integer entries correspond to computational basis states
+                -> List entries correspond to operators where the eigenvalue 
+                   is dependent on the integer entry eigenvalues.
+        """
+        self.ansatz = ansatz
+        self.basis = [] # Operators that map to the eigenstates (comp basis)
+        self.rest = []  # Operators that depend on the eigenstate operators
+        for outcome in outcomes:
+            if isinstance(outcome,list):
+                self.rest.append(outcome)
+            else:
+                self.basis.append(outcome)
+
+    def expectation(self,result,shots):
+        print(result)
+        E = np.zeros(len(self.paulis))
+        for state,num_measure in result.items():
+            state = state[::-1]
+            eigvals = np.zeros(len(self.basis))
+            for op,ind in enumerate(self.basis):
+                eigval = 1 if state[ind] == '0' else -1
+                eigvals[op] = eigval
+                E[op] += eigval*num_measure*self.paulis[op].factor
+            for i,elem in enumerate(self.rest):
+                op = len(self.basis)+i
+                eigval = 1
+                for e in elem:
+                    if not isinstance(e,str):
+                        eigval *= eigvals[e]
+                    else:
+                        if e == '-':
+                            eigval *= -1
+                #print(state,num_measure,eigval*num_measure*self.paulis[op].factor/shots,self.paulis[op])
+                E[op] += eigval*num_measure*self.paulis[op].factor
+        print(E/shots)
+        return np.sum(E)/shots
+
+    def __len__(self):
+        return len(self.paulis)
+
+    def __eq__(self,other):
+        return False
+
+class CircuitList:
+    def __init__(self):
+        self.circuits = []
+
+    def append(self,circuit,check_equal=False):
+        if check_equal:
+            if circuit in self.circuits:
+                i = self.circuits.index(circuit)
+                self.circuits[i].factor += circuit.factor
+            else:
+                self.circuits.append(circuit)
+        else:
+            self.circuits.append(circuit)
+        #self.circuits.append(circuit)
+
+    def groupz(self):
+        z_group = QWCGroup()
+        for circ in self.circuits:
+            if X() not in circ.gates and Y() not in circ.gates:
+                self.circuits.remove(circ)
+                z_group.append(circ)
+        self.circuits.insert(0,z_group)
+
+    def __getitem__(self,i):
+        return self.circuits[i]
+    
+    def __str__(self):
+        string = ''
+        for elem in self.circuits:
+            string += str(elem)+'\n'
+        return string[:-1]
+
+    def __len__(self):
+        return len(self.circuits)
+
+
--- quantum_circuit/hamiltonian.py
+++ quantum_circuit/hamiltonian.py
@@ -1,9 +1,10 @@
 import numpy as np
 from .circuit import QuantumCircuit
 from .utils import molecular2sec_quant
-from .gates import X,Y,Rotation,Ph
+from .gates import X,Y,Z,Rotation,Ph
+from .circuit_list import CircuitList,PauliString,QWCGroup
 
-class SecondQuantizedHamiltonian:
+class Hamiltonian:
     def __init__(self,n,l,
                  one_body,two_body,exp=False,
                  nuclear_repulsion=None,anti_symmetric=False,add_spin=False,
@@ -29,8 +30,117 @@ class SecondQuantizedHamiltonian:
         self.exp = exp
         self.conv = conv 
 
+
+class FermionHamiltonian(Hamiltonian):
+    def __init__(self,n,l,
+                 one_body,two_body,exp=False,
+                 nuclear_repulsion=0,anti_symmetric=False,add_spin=False,
+                 conv=1):
+
+        super().__init__(n,l,one_body,two_body,exp,
+                         nuclear_repulsion,anti_symmetric,add_spin,conv)
+
+        self.circuit_list = CircuitList()
+        self.get_circuit()
+
+    def get_circuit(self):
+        # make x,y,z gate instances
+        x,y,z = X(),Y(),Z()
+        # Add nuclear repulsion to constant factor (can be 0)
+        constant = self.nuclear_repulsion
+        Zgroup = QWCGroup()
+        # One-body interactions
+        for p in range(self.l):
+            if not np.isclose(self.h[p,p],0):
+                factor = 0.5*self.h[p,p]
+                constant += factor
+                Zgroup.append(PauliString(-factor,[Z()],[p]),check_equal=True)
+            for q in range(p+1,self.l):
+                if not np.isclose(self.v[p,q,p,q],0):
+                    factor = 0.25*self.v[p,q,p,q]
+                    constant += factor
+                    Zgroup.append(PauliString(-factor,[Z()],[p]),check_equal=True)
+                    Zgroup.append(PauliString(-factor,[Z()],[q]),check_equal=True)
+                    Zgroup.append(PauliString(factor,[Z(),Z()],[p,q]))
+        self.circuit_list.append(PauliString(constant,[],[]))
+        #for pauli in Zgroup:
+        #    self.circuit_list.append(pauli)
+        self.circuit_list.append(Zgroup)
+
+        # Two-body interactions
+        for p in range(self.l):
+            for q in range(p+1,self.l):
+                for r in range(q+1,self.l):
+                    for s in range(r+1,self.l):
+                        factor1 = 0.125*(self.v[p,q,r,s]\
+                                       + self.v[p,r,q,s]\
+                                       + self.v[p,r,s,q])
+                        factor2 =-0.125*(self.v[p,q,r,s]\
+                                       - self.v[p,r,q,s]\
+                                       - self.v[p,r,s,q])
+                        factor3 = 0.125*(self.v[p,q,r,s]\
+                                       + self.v[p,r,q,s]\
+                                       - self.v[p,r,s,q])
+                        factor4 = 0.125*(self.v[p,q,r,s]\
+                                       - self.v[p,r,q,s]\
+                                       + self.v[p,r,s,q])
+                        # Make Pauli strings
+                        pqrs = [p,q,r,s]
+                        xxxx = [x,x,x,x]
+                        yyyy = [y,y,y,y]
+                        xxyy = [x,x,y,y]
+                        yyxx = [y,y,x,x]
+                        xyyx = [x,y,y,x]
+                        yxxy = [y,x,x,y]
+                        xyxy = [x,y,x,y]
+                        yxyx = [y,x,y,x]
+                        # Insert Z gates between p and q, and r and s
+                        for k in range(r+1,s):
+                            pqrs.insert(3,k)
+                            xxxx.insert(3,z)
+                            yyyy.insert(3,z)
+                            xxyy.insert(3,z)
+                            yyxx.insert(3,z)
+                            xyyx.insert(3,z)
+                            yxxy.insert(3,z)
+                            xyxy.insert(3,z)
+                            yxyx.insert(3,z)
+                        for k in range(p+1,q):
+                            pqrs.insert(1,k)
+                            xxxx.insert(1,z)
+                            yyyy.insert(1,z)
+                            xxyy.insert(1,z)
+                            yyxx.insert(1,z)
+                            xyyx.insert(1,z)
+                            yxxy.insert(1,z)
+                            xyxy.insert(1,z)
+                            yxyx.insert(1,z)
+
+                        if not np.isclose(factor1,0):
+                            self.circuit_list.append(PauliString(factor1,xxxx,pqrs),check_equal=True)
+                            self.circuit_list.append(PauliString(factor1,yyyy,pqrs),check_equal=True)
+                        if not np.isclose(factor2,0):
+                            self.circuit_list.append(PauliString(factor2,xxyy,pqrs),check_equal=True)
+                            self.circuit_list.append(PauliString(factor2,yyxx,pqrs),check_equal=True)
+                        if not np.isclose(factor3,0):
+                            self.circuit_list.append(PauliString(factor3,xyyx,pqrs),check_equal=True)
+                            self.circuit_list.append(PauliString(factor3,yxxy,pqrs),check_equal=True)
+                        if not np.isclose(factor4,0):
+                            self.circuit_list.append(PauliString(factor4,xyxy,pqrs),check_equal=True)
+                            self.circuit_list.append(PauliString(factor4,yxyx,pqrs),check_equal=True)
+
+
+class SecondQuantizedHamiltonian(Hamiltonian):
+    def __init__(self,n,l,
+                 one_body,two_body,exp=False,
+                 nuclear_repulsion=None,anti_symmetric=False,add_spin=False,
+                 conv=1):
+        super().__init__(n,l,one_body,two_body,exp,
+                         nuclear_repulsion,anti_symmetric,add_spin,conv)
+
         self.circuit = []
         self.get_circuit()
+        #self.circuit_list = self.to_circuit_list('vqe')
 
     def get_circuit(self):
         circuits = []
@@ -41,21 +151,28 @@ class SecondQuantizedHamiltonian:
             circuits.append(rep)
         # One-body interactions
         for p in range(self.l):
-            for q in range(self.l):
-                if not np.isclose(self.h[p,q],0):
-                    qc = QuantumCircuit(self.l)
-                    qc.insert_one_body_operator(self.h[p,q],p,q,
-                                                conv=self.conv)
-                    circ = qc.transform_ladder_operators()
-                    circuits += circ
+            if not np.isclose(self.h[p,p],0):
+                qc = QuantumCircuit(self.l)
+                qc.insert_one_body_operator(self.h[p,p],p,p,
+                                            conv=self.conv)
+                circ = qc.transform_ladder_operators()
+                circuits += circ
+        #for p in range(self.l):
+        #    for q in range(self.l):
+        #        if not np.isclose(self.h[p,q],0):
+        #            qc = QuantumCircuit(self.l)
+        #            qc.insert_one_body_operator(self.h[p,q],p,q,
+        #                                        conv=self.conv)
+        #            circ = qc.transform_ladder_operators()
+        #            circuits += circ
         # Two-body interactions
         for i in range(self.l):
-            for j in range(self.l):
-                for b in range(self.l):
-                    for a in range(self.l):
-                        if not np.isclose(self.v[i,j,b,a],0):
+            for j in range(i+1,self.l):
+                for a in range(self.l):
+                    for b in range(a+1,self.l):
+                        if not np.isclose(self.v[i,j,a,b],0):
                             qc = QuantumCircuit(self.l)
-                            qc.insert_two_body_operator(self.v[i,j,b,a],i,j,b,a,
+                            qc.insert_two_body_operator(4*self.v[i,j,a,b],i,j,a,b,
                                                         conv=self.conv)
                             circ = qc.transform_ladder_operators()
                             circuits += circ
@@ -63,7 +180,6 @@ class SecondQuantizedHamiltonian:
         self.circuit = self.get_unique(circuits)
 
     def get_unique(self,circuits):
-        #for i in circuits: print(repr(i))
         for i in reversed(range(len(circuits))):
             circ = circuits[i]
             circ.gate_optimization()
@@ -95,22 +211,19 @@ class SecondQuantizedHamiltonian:
             final_circ.gate_optimization()
         return final_circ
 
-    def to_circuit_list(self,ptype='vqe'):
+    def circuit_list(self,algorithm):
         """
-        Input:
-            - ptype (str): How to represent gate actions
-                - qiskit -> As qiskit gates (to be appended in a qiskit.QuantumCircuit)
-                - vqe    -> As Qoperator in VQE takes in
-                - openfermion -> As openfermion prints.
+        Return circuit list to specific algorithm.
         """
-        circuit_list = []
-        if isinstance(self.circuit,QuantumCircuit):
+        if algorithm.lower() == 'qpe':
+            circuit_list = []
+            assert isinstance(self.circuit,QuantumCircuit)
             # single gate represented as [char,factor,qubit(,2nd qubit)]
             # with factor = 1 if not rotation gate
             control_list = self.circuit.register.control_list
             register = self.circuit.register
             for i,gate in enumerate(control_list):
-                if gate.is_identity(): # Not control gates
+                if gate.is_identity(): # No control gates
                     for j,qbit in enumerate(register):
                         if i < len(qbit):
                             if not qbit[i].is_identity(): # No need to add identity gates
@@ -130,26 +243,18 @@ class SecondQuantizedHamiltonian:
                     else:
                         circuit_list.append([gate,1,gate.c,gate.t])
                     
+        elif algorithm.lower() == 'vqe':
+            circuit_list = CircuitList()
+            for circ in self.circuit:
+                gates = []
+                qubits = []
+                for i,qbit in enumerate(circ.register):
+                    if len(qbit) == 1:
+                        gates.append(qbit[0])
+                        qubits.append(i)
+                circuit_list.append(PauliString(circ.factor,gates,qubits))
+            circuit_list.groupz()
         else:
-            for circuit in self.circuit:
-                circuit.defactor() # not necessary?
-                new = [circuit.factor]
-                for i,qbit in enumerate(circuit.register.qubits):
-                    for gate in qbit.circ:
-                        if not gate.is_identity():
-                            if ptype == 'qiskit':
-                                new.append([i,gate.to_qiskit()])
-                            elif ptype == 'vqe':
-                                new.append([i,gate.char.lower()])
-                            elif ptype == 'openfermion':
-                                new.append('{}{}'.format(gate.char,i))
-                #if len(new) == 1:
-                #    new.append([])
-                circuit_list.append(new)
+            raise ValueError('Input either "vqe" or "qpe"')
         return circuit_list
-
-
-class ExponentialHamiltonian:
-    def __init__(self,n,l):
-        pass
 
