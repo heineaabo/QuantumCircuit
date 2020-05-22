1b6d4a3a653d769e4411dac489548ca6819c0fc5
Heine Olsson Aabø <heine2307@eduroam-193-157-161-138.wlan.uio.no>
2020-05-22 11:02:23 +0200

    add circuit list classes

--- quantum_circuit/__init__.py
+++ quantum_circuit/__init__.py
@@ -1,7 +1,10 @@
 from .circuit import QuantumCircuit
 from .register import QuantumRegister
 from .qubit import Qubit
-from .hamiltonian import SecondQuantizedHamiltonian,ExponentialHamiltonian
+from .hamiltonian import SecondQuantizedHamiltonian,FermionHamiltonian
+
+from .circuit_list import CircuitList,PauliString,QWCGroup,GCGroup
+
 from quantum_circuit.gates import *
 from quantum_circuit.utils import *
 from quantum_circuit.optimization import *
